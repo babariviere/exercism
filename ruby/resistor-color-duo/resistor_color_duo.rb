973a4396ec5aee0ad8270bb12ddfa301c8ebaa5f
Bastien Rivière <babathriviere@gmail.com>
2020-03-29 16:59:56 +0200

    ruby: resistor color duo (3rd iter)

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,5 +1,5 @@
 class ResistorColorDuo
-  @@colors = {
+  COLORS = {
     'black' => 0,
     'brown' => 1,
     'red' => 2,
@@ -10,11 +10,11 @@ class ResistorColorDuo
     'violet' => 7,
     'grey' => 8,
     'white' => 9
-  }
+  }.freeze
 
   def self.value(colors)
     colors.take(2).reduce(0) do |acc, color|
-      acc * 10 + @@colors[color]
+      acc * 10 + COLORS[color]
     end
   end
 end
