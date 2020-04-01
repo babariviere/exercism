b826df671a32122e8abb8f3d3c353fe07991e72a
Bastien Rivière <babathriviere@gmail.com>
2020-04-01 21:15:23 +0200

    haskell: pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,15 +1,7 @@
 module Pangram (isPangram) where
 
-import qualified Data.HashSet as Set
 import Data.Char
+import Data.List
 
 isPangram :: String -> Bool
-isPangram text | length text < 26 = False
-               | otherwise = Set.size(getLetters Set.empty text) == 26
-
-getLetters :: Set.HashSet Char -> String -> Set.HashSet Char
-getLetters set [] = set
-getLetters set (x : xs) | Set.size set == 26 = set
-                        | x `elem` ['a'..'z'] = getLetters (Set.insert x set) xs
-                        | x `elem` ['A'..'Z'] = getLetters (Set.insert (toLower x) set) xs
-                        | otherwise = getLetters set xs
+isPangram = (==26) . length . nub . filter (`elem` ['a'..'z']) . map toLower
